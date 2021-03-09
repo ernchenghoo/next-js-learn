13987f42b15671b8c5a90a3aad3ec2bf88c9dc66
Ern Cheng <erncheng@inglab.com.my>
2021-03-09 10:33:51 +0800

    first commit

---
+++ pages/login.js
@@ -0,0 +1,33 @@
+import Head from 'next/head'
+import Layout from '../components/layout'
+import Link from 'next/link'
+
+export default function Login() {
+    return (
+        <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+    </Layout>
+      )
+    
+    
+}
+
+
+const YourComponent = () => (
+    <Image
+      src="/images/profile.jpg" // Route of the image file
+      height={144} // Desired size with correct aspect ratio
+      width={144} // Desired size with correct aspect ratio
+      alt="Your Name"
+    />
+  )
+
+  
